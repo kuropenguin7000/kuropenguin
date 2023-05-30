82148a6a9ee5a1da3de5d0eea27442fddf327386
rafi.rahman <rafi.rahman@gdn-commerce.com>
2023-05-30 14:49:49 +0700

    init new fire base, add countdown, change title, change icon

--- src/main.js
+++ src/main.js
@@ -3,5 +3,10 @@ import store from '@/store'
 import router from '@/router'
 import App from '@/App.vue'
 import '@/assets/main.scss'
+import Countdown from 'vue3-flip-countdown'
 
-createApp(App).use(store).use(router).mount('#app')
+createApp(App)
+  .use(store)
+  .use(router)
+  .use(Countdown)
+  .mount('#app')
